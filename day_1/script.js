2dd7c9fe24eea1e0c2800013098ce329707a11b9
Sameer-Sonara <Sameer.Sonara@zeuslearning.com>
2025-06-05 19:21:36 +0530

    day3

--- day_1/script.js
+++ day_1/script.js
@@ -1,20 +1,40 @@
 function validateForm(event) {
     event.preventDefault();
-    const name = document.getElementById("name").value.trim();
-    const comments = document.getElementById("comments").value.trim();
+
+    const name = document.getElementById("name");
+    const comments = document.getElementById("comments");
     const male = document.getElementById("male").checked;
     const female = document.getElementById("female").checked;
+    
+    const error = document.getElementsByClassName("error");
 
-    if (name === "" || comments === "") {
-        alert("Please fill out both fields.");
+    for(var i = 0 ; i<error.length ; i++)
+    error[i].innerHTML="";
+    
+    
+    if (name.value.trim() === "" )
+    {
+        // alert("Please fill your name");
+        name.focus();
+        error[0].innerHTML="Please fill you name";
         return false;
+
+    }
+    if(comments.value.trim() === "") 
+    {
+        // alert("Please add your comments");
+        comments.focus();
+        error[1].innerHTML="Please fill you Comments";
+        return false;        
     }
 
     if (!male && !female) {
-        alert("Please select your gender.");
+        // alert("Please select your gender.");
+        error[2].innerHTML="Please select your gender";
         return false;
     }
-
+    for(var i = 0 ; i<error.length ; i++)
+    error[i].innerHTML="";
     alert("Form submitted successfully!");
     return true;
 }
---
+++ day_3/fact.js
@@ -0,0 +1,61 @@
+let ans = [1]; 
+// console.log("hello");
+
+const N = 100000000;
+function multi(a, b) 
+{
+    let res = Array(a.length + b.length).fill(0);
+
+    for (let i = 0; i < a.length; i++) 
+        {
+        for (let j = 0; j < b.length; j++) 
+            {
+                res[i + j] += a[i] * b[j];
+                if (res[i + j] >= N) 
+                {
+                    res[i + j + 1] += Math.floor(res[i + j] / N);
+                    res[i + j] %= N;
+                }
+        }
+    }
+
+    while (res.length > 1 && res[res.length - 1] === 0) 
+        {
+            res.pop();
+        }
+
+    return res;
+}
+
+function fact(x) 
+{
+    ans = [1];  
+    for (let i = 2; i <= x; i++) 
+        {
+            let temp = [];
+            let num = i;
+            while (num > 0) 
+                {
+                    temp.push(num % N);
+                    num = Math.floor(num / N);
+                }
+            ans = multi(ans, temp);
+    }
+
+    return ans;
+}
+
+
+console.time("Time: ");
+var factorial = fact(10e10);
+
+var str = factorial[factorial.length - 1].toString();
+for(let i = factorial.length-2 ; i>=0 ; i--)
+{
+    str += factorial[i].toString().padStart(8,"0");   
+}
+console.timeEnd("Time: ");
+
+console.log(factorial);  // Output: "120"
+console.log(str);
+console.log("hello");
